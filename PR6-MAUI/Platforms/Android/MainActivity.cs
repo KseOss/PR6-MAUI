763b96a80f4ede7baaf45b1c2b680946ee528c2a
KseOss <104015053+KseOss@users.noreply.github.com>
2025-10-20 10:40:26 +0300

    Update MainActivity.cs

--- PR6-MAUI/Platforms/Android/MainActivity.cs
+++ PR6-MAUI/Platforms/Android/MainActivity.cs
@@ -7,5 +7,18 @@ namespace PR6_MAUI
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+    protected override void OnCreate(Bundle? savedInstanceState)
+{
+    base.OnCreate(savedInstanceState);
+    const int requestNotifictation = 0;
+    string[] notiPermission = 
+        { 
+        Manifest.Permission.PostNotifications
+    };
+    if ((int)Build.VERSION.SdkInt < 33)
+        return;
+    if (CheckSelfPermission(Manifest.Permission.PostNotifications) == Permission.Granted)
+        return;
+    RequestPermissions(notiPermission, requestNotifictation);
     }
 }
